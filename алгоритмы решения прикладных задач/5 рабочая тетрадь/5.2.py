8604f84eb0f4c17803967fd7ce03d611736aafc3
twisted_none <dema.shvetsov@gmail.com>
2023-11-20 14:38:25 +0300

    5.2 update

--- алгоритмы решения прикладных задач/5 рабочая тетрадь/5.2.py
+++ алгоритмы решения прикладных задач/5 рабочая тетрадь/5.2.py
@@ -2,9 +2,7 @@
 # Xn=Xn-1+Xn-2;    X0=1;   X1=1 (см. подсказки в слайдах с лекции)
 
 def fib(n):
-    if n == 0:
-        return 1
-    elif n == 1:
+    if n in (0, 1):
         return 1
     return fib(n - 1) + fib(n - 2)
 
